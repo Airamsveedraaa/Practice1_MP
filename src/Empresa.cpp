5a9f9997523119332ca9e4e18ac97eb25f4ff5c2
Airamsveedraaa <l.saavedrap5@gmail.com>
2025-10-28 13:54:11 +0100

    Added methods 'BajaContrato' and 'BajaCliente'

--- src/Empresa.cpp
+++ src/Empresa.cpp
@@ -28,17 +28,19 @@ Empresa::~Empresa()
 int Empresa::buscarCliente(long int dni)const
 {
 
-int pos=-1;
-int i=0;
-bool existe=false;
-while(i<ncli && !existe){
-    if(this->clientes[i]->getDni()==dni){
-        pos=i;
-        existe=true;
+    int pos=-1;
+    int i=0;
+    bool existe=false;
+    while(i<ncli && !existe)
+    {
+        if(this->clientes[i]->getDni()==dni)
+        {
+            pos=i;
+            existe=true;
+        }
+        i++;
     }
-    i++;
-}
-return pos;
+    return pos;
 }
 
 int Empresa::altaCliente(Cliente *c)
@@ -53,7 +55,7 @@ int Empresa::altaCliente(Cliente *c)
     }
     else
         cout << "Lo siento, cupo de clientes lleno";
-             pos=-1;
+    pos=-1;
 
     return pos;
 }
@@ -65,7 +67,9 @@ void Empresa::crearContrato()
     cin >> dni;
 
     int pos1=buscarCliente(dni);
-    if(pos1==-1){
+    if(pos1==-1)
+    {
+        //El cliente no ha sido dado de alta, se hace manualmente
 
         int dia,mes,anio;
         char nombre[100];
@@ -77,63 +81,141 @@ void Empresa::crearContrato()
         C=new Cliente(dni,nombre,Fecha(dia,mes,anio));
         int pos2=this->altaCliente(C);
 
-        if(pos2=!-1){
-        char contrato;
-        cout << "Que tipo de contrato desea abrir, Tarifa Plana (T) o Movil (M)?";
-        cin >> contrato;
-        if(toupper(contrato)=='T'){
-            int m,dia,mes,anio;
-            ContratoTP *CP;
-            cout << "Introduzca los minutos que quiere para llamadas: ";
-            cin >> m;
-            cout << "Introduzca el dia,mes y anio del contrato:";
-            cin >> dia,mes,anio;
-            CP=new ContratoTP(dni,Fecha(dia,mes,anio),m);
+        if(pos2=!-1)
+        {
+            //Si la posicion es distinta de -1 quiere decir que se ha podido dar de alta en la posicion pos2
+            //Pasamos a preguntar el tipo de contrato
+            char contrato;
+            cout << "Que tipo de contrato desea abrir, Tarifa Plana (T) o Movil (M)?";
+            cin >> contrato;
+            if(toupper(contrato)=='T')
+            {
+                int m,dia,mes,anio;
+                ContratoTP *CP;
+                cout << "Introduzca los minutos que quiere para llamadas: ";
+                cin >> m;
+                cout << "Introduzca el dia,mes y anio del contrato:";
+                cin >> dia,mes,anio;
+                CP=new ContratoTP(dni,Fecha(dia,mes,anio),m);
 
-            //Reasignacion de tamano si esta lleno
-            if(this->ncon==this->nmaxcon){
-                nmaxcon*=2;
-                Contrato **Aux=new Contrato*[ncon*2];
-                for(int i=0; i<ncon;i++){
-                    contratos[i]=Aux[i];
+                //Reasignacion de tamano si esta lleno
+                if(this->ncon==this->nmaxcon)
+                {
+                    nmaxcon*=2;
+                    Contrato **Aux=new Contrato*[ncon*2];
+                    for(int i=0; i<ncon; i++)
+                    {
+                        contratos[i]=Aux[i];
+                    }
+
+                    delete [] Aux;
                 }
-
-                delete [] Aux;
+                this->contratos[ncon++]=CP;
             }
-            this->contratos[ncon++]=CP;
-        }
-        else if (toupper(contrato)=='M'){
-            float precio;
-            int m,dia,mes,anio;
-            char* nacionalidad;
+            else if (toupper(contrato)=='M')
+            {
+                float precio;
+                int m,dia,mes,anio;
+                char* nacionalidad;
 
-            cout << "Dime el precio del contrato: ";
-            cin >> precio;
-            cout << "Dime el dia,mes y anio del contrato:";
-            cin >> dia,mes,anio;
-            cout << "Dime los minutos hablados:";
-            cin >> m;
-            cin.ignore();
-            cout << "Dime la nacionalidad del cliente:";
-            cin.getline(nacionalidad,100);
+                cout << "Dime el precio del contrato: ";
+                cin >> precio;
+                cout << "Dime el dia,mes y anio del contrato:";
+                cin >> dia,mes,anio;
+                cout << "Dime los minutos hablados:";
+                cin >> m;
+                cin.ignore();
+                cout << "Dime la nacionalidad del cliente:";
+                cin.getline(nacionalidad,100);
 
-            ContratoMovil *CM;
-            CM=new ContratoMovil(dni,Fecha(dia,mes,anio),precio,m,nacionalidad);
- //Reasignacion de tamano si esta lleno
-            if(this->ncon==this->nmaxcon){
-                nmaxcon*=2;
-                Contrato **Aux2=new Contrato*[ncon*2];
-                for(int i=0; i<ncon;i++){
-                    contratos[i]=Aux2[i];
+                ContratoMovil *CM;
+                CM=new ContratoMovil(dni,Fecha(dia,mes,anio),precio,m,nacionalidad);
+                //Reasignacion de tamano si esta lleno
+                if(this->ncon==this->nmaxcon)
+                {
+                    nmaxcon*=2;
+                    Contrato **Aux2=new Contrato*[ncon*2];
+                    for(int i=0; i<ncon; i++)
+                    {
+                        contratos[i]=Aux2[i];
+                    }
+
+                    delete [] Aux2;
                 }
-
-                delete [] Aux2;
+                this->contratos[ncon++]=CM;
             }
-            this->contratos[ncon++]=CM;
         }
     }
-    }
-    else{
+    else
+    {
         cout << "El cliente ya ha sido dado de alta en el sistema";
     }
+}
+
+
+bool Empresa::cancelarContrato(int idContrato)
+{
+
+    bool encontrado=true;
+
+    int i=0;
+
+    while(i<ncon && !encontrado)
+    {
+        if(contratos[i]->getIdContrato()==idContrato)
+        {
+            encontrado=true;
+            delete this->contratos[i];
+            for(int j=i+1; j<ncon; j++)
+            {
+                contratos[j-1]=contratos[j];
+            }
+            ncon--;
+        }
+        i++;
+    }
+
+    if(ncon < nmaxcon/2)
+    {
+        nmaxcon/=2;
+        Contrato **Aux=new Contrato*[nmaxcon];
+        for(int i=0; i<ncon; i++)
+        {
+            Aux[i]=contratos[i];
+        }
+        delete [] this->contratos;
+        contratos=Aux;
+    }
+
+    return encontrado;
+
+}
+
+
+bool Empresa::bajaCliente(long int dni)
+{
+
+    bool encontrado=false;
+    int i=0;
+
+    //primero dar de baja el contrato de dicho cliente
+    while(i<ncon){
+        if(contratos[i]->getDniContrato()==dni){
+            cancelarContrato(contratos[i]->getIdContrato());
+        }
+        i++;
+    }
+
+    while(i<ncli && !encontrado)
+    {
+        if(clientes[i]->getDni()==dni)
+        {
+            encontrado=true;
+            delete this->clientes[i];
+            ncli--;
+        }
+        i++;
+    }
+
+    return encontrado;
 }
